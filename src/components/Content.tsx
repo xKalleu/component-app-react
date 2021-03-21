8cb64215af389b2fd662b7dafefe4c5df1ab9c7b
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-21 15:25:33 -0300

    add: finish challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
-  // Complete aqui
+
+import { Header } from "./Header"
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+interface MovieProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+  selectedGenre: string;
+}
+
+export function Content({movies, selectedGenre} : MovieProps) {
+  return (
+    <div className="container">
+      <header>
+        <Header title={selectedGenre}/>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
